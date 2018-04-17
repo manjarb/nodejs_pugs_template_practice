05d67a86d471a236ab5ebaa670d8dd7f1e9744ce
Varis <manjarb4@gmail.com>
2018-04-18 01:52:28 +0800

    Update async controller

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 /*exports.myMiddleware = (req, res, next) => {
     req.name = 'wes';
     if(req.name === 'wes') {
@@ -23,6 +26,18 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body)
+exports.createStore = async (req, res) => {
+    // res.json(req.body)
+    const store = new Store(req.body);
+    // Traditional Promise
+    /*store
+        .save()
+        .then(store => {
+            res.json(store);
+        })
+        .catch(err => {
+            throw Error(err);
+        })*/
+    await store.save();
+    res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 // router.get('/', storeController.myMiddleware, storeController.homePage);
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 router.get('/reverse/:name', (reg, res) => {
   const reverse = [...reg.params.name].reverse().join('');
