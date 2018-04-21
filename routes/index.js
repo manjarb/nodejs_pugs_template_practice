276f5f3d7dc96dad882f51553bf82ae411902d03
Varis <manjarb4@gmail.com>
2018-04-22 03:53:13 +0800

    Update register user form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -30,5 +31,21 @@ router.get('/reverse/:name', (reg, res) => {
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag*?', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register',
+    userController.validateRegister,
+    // we need to know about errors if
+    // validation will be passed, but registration
+    // will be failed in some reasons, e.g. second
+    // registration with same email
+    catchErrors(userController.register),
+    authController.login
+);
 
 module.exports = router;
